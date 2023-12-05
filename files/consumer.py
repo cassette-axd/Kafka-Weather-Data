77a467af9e50f08584c60542d8627585946e4600
Casey Reyes <ccreyes@wisc.edu>
2023-12-05 04:48:34 +0000

    created atomic writes function

--- files/consumer.py
+++ files/consumer.py
@@ -2,6 +2,12 @@ from kafka import KafkaConsumer, TopicPartition
 import json
 import sys
 
+def write_atomic(data, path):
+    path2 = path + ".tmp"
+    with open(tmp_path, "w") as f:
+        json.dump(data, f)
+    os.replace(tmp_path, path)
+
 partitions = [int(partition) for partition in sys.argv[1:]]
 partitions.sort()
 
@@ -15,7 +21,7 @@ try:
     partition_data = {partition: {"partition": partition, "offset": 0} for partition in partitions}
     for partition in partitions:
         # Check if partition-N.json exists
-        path = f'partition-{partition}.json'
+        filename = f'/files/partition-{partition}.json'
         try:
             with open(filename, 'r') as f:
                 partition_data[partition] = json.load(f)
@@ -25,25 +31,20 @@ try:
             offset = 0
             # with open(filename, 'w') as f:
             #     json.dump(partition_data[partition], f)
-            path2 = path + ".tmp"
-            with open(path2, "w") as f:
-                json.dump(partition_data[partition], f)
-            os.replace(path2, path)
+            write_atomic(partition_data[partition], filename)
+
         # Seek to the specified offset
         consumer.seek(TopicPartition(topic, partition), partition_data[partition]['offset'])
 
     while True:
         batch = consumer.poll(1000)
         for partition, messages in batch.items():
-            path = f'partition-{partition}.json'
+            filename = f'/files/partition-{partition}.json'
             # partition_data = {"partition": partition, "offset": partition.position().offset}
             # with open(filename, 'w') as f:
             #     json.dump(partition_data, f)
             partition_data[partition] = {"partition": partition, "offset": messages[-1].offset + 1}
-            path2 = path + ".tmp"
-            with open(path2, "w") as f:
-                json.dump(partition_data[partition], f)
-            os.replace(path2, path)
+            write_atomic(partition_data[partition], filename)
 except: KeyboardInterrupt:
     pass
 finally:
