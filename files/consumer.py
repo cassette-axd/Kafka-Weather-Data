a0607e93f5cb07aea39b50b2861f027fee7ef6fd
Casey Reyes <ccreyes@wisc.edu>
2023-12-05 04:40:33 +0000

    completed atomic writes

--- files/consumer.py
+++ files/consumer.py
@@ -15,7 +15,7 @@ try:
     partition_data = {partition: {"partition": partition, "offset": 0} for partition in partitions}
     for partition in partitions:
         # Check if partition-N.json exists
-        filename = f'partition-{partition}.json'
+        path = f'partition-{partition}.json'
         try:
             with open(filename, 'r') as f:
                 partition_data[partition] = json.load(f)
@@ -23,18 +23,27 @@ try:
             # Initialize partition-N.json if it does not exist
             partition_data = {"partition": partition, "offset": 0}
             offset = 0
-            with open(filename, 'w') as f:
+            # with open(filename, 'w') as f:
+            #     json.dump(partition_data[partition], f)
+            path2 = path + ".tmp"
+            with open(path2, "w") as f:
                 json.dump(partition_data[partition], f)
+            os.replace(path2, path)
         # Seek to the specified offset
         consumer.seek(TopicPartition(topic, partition), partition_data[partition]['offset'])
 
     while True:
         batch = consumer.poll(1000)
-        for partition in batch.items():
-            filename = f'partition-{partition}.json'
-            partition_data = {"partition": partition, "offset": partition.position().offset}
-            with open(filename, 'w') as f:
-                json.dump(partition_data, f)
+        for partition, messages in batch.items():
+            path = f'partition-{partition}.json'
+            # partition_data = {"partition": partition, "offset": partition.position().offset}
+            # with open(filename, 'w') as f:
+            #     json.dump(partition_data, f)
+            partition_data[partition] = {"partition": partition, "offset": messages[-1].offset + 1}
+            path2 = path + ".tmp"
+            with open(path2, "w") as f:
+                json.dump(partition_data[partition], f)
+            os.replace(path2, path)
 except: KeyboardInterrupt:
     pass
 finally:
